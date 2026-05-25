a12469c3eefff4ab2aa74765a284bd1056f3dd16
manish4umar <jimanish.official@gmail.com>
2026-05-25 09:40:36 +0530

    new files

---
+++ Odd.py
@@ -0,0 +1,4 @@
+num = int (input("Enter Number : "))
+
+if num%2 == 0:(print("Even"))
+else : (print ("Odd"))
---
+++ Sum.py
@@ -0,0 +1,7 @@
+First=float(input("Enter First No :- "))
+
+Second=int(input("Enter Second No :-"))
+
+sum = First + Second
+
+print("Sum Of there Number are :- " , sum)
